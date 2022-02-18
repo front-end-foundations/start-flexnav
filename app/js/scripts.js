3a671b8f5c891e58966dc8cfe82a43174720dccd
Daniel Deverell <daniel.deverell@gmail.com>
2022-02-17 20:23:02 -0600

    done for now

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -1,29 +1,33 @@
 var tabs = document.querySelectorAll('nav a')
 var contentPara = document.querySelector('.content')
 
-// tabs.forEach((tab) => tab.addEventListener('click', makeActive))
-document.addEventListener('click', makeActive)
-
-function makeActive(event) {
-  if (!event.target.matches('a')) return // NEW
-  console.log(event.target)
-  makeInactive()
-  event.target.classList.add('active')
-
-  if (event.target.href.includes('cuisines')) {
-    contentPara.innerHTML = data.cuisines
-  } else if (event.target.href.includes('chefs')) {
-    contentPara.innerHTML = data.chefs
-  } else if (event.target.href.includes('reviews')) {
-    contentPara.innerHTML = data.reviews
-  } else if (event.target.href.includes('delivery')) {
-    contentPara.innerHTML = data.delivery
-  }
-  event.preventDefault()
+// when the hash changes
+function setActiveTabAccordingToHash(type) {
+  makeAllTabsInactive()
+  var tabToActivate = document.querySelector(`a[href="#${type}"]`)
+  tabToActivate.classList.add('active')
 }
 
-function makeInactive() {
+function makeAllTabsInactive() {
   tabs.forEach((tab) => tab.classList.remove('active'))
 }
 
-contentPara.innerHTML = data.cuisines
+// runs on page load and whenever the hash changes
+function setContentAccordingToHash() {
+  var type = window.location.hash.substring(1)
+  contentPara.innerHTML = data[type]
+  setActiveTabAccordingToHash(type)
+}
+
+// only runs once on page load
+function initializePage() {
+  if (!window.location.hash) {
+    window.location.hash = 'cuisines'
+    document.querySelector('[href="#cuisines"]').classList.add('active')
+  }
+  setContentAccordingToHash()
+}
+
+window.addEventListener('hashchange', setContentAccordingToHash)
+
+initializePage()
